67a9a0bb064a988361edd8714393f18274865371
nguyenminhduc2c19 <nguyenminhduc2c19@gmail.com>
2024-09-12 16:05:31 +0700

    .

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const fs = require("fs");
 const path = require("path");
 const app = express();
 const port = 3000;
-
+app.use(cors());
 // Middleware to parse JSON bodies
 app.use(express.json());
 
